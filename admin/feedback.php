02a0206b2598e66d42352070f30228b2e9be534d
VijayKumar <22suit17@tcarts.in>
2024-09-15 13:05:49 +0530

    Now adding

--- admin/feedback.php
+++ admin/feedback.php
@@ -70,8 +70,4 @@
 </html>
 <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
 <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
-<script type="text/javascript">
-    if ( window.history.replaceState ) {
-window.history.replaceState( null, null, window.location.href );
-}
-  </script>
+<script src="js/script.js"></script>
